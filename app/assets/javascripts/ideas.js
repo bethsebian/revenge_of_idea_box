2dae23c25dc3bc6ed63279ae05738f4e44989cf8
Beth Sebian <esebian@gmail.com>
2016-03-22 16:51:32 -0600

    Implement javascript to list all ideas on page

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -6,37 +6,14 @@ function renderIdea(idea) {
   return $('<div><h2>' + idea.title + '</h2><p>' + idea.body +'</p></div>').addClass('idea');
 }
 
-function listAllIdeas(){
-  $.ajax({
-    type: 'GET',
-    url: '/api/v1/ideas.json',
-    success: function(ideas){
-      var target = $('.ideas_list');
-      $(target).append('Hello');
-      // return('Hello');
-
-      // $(".ideas_list");
-      // ideas.map(renderIdea);
-      // $(target).append(renderedIdeas);
-      //iterate through the ideas
-      //format a string that looks like html with our idea info per idea
-      //use jquery to append our artisinal html to the page
-    },
-    error: function(){
-      //semi optional - won't explode but will silently fail
-    }
-  });
+function collectAndFormatIdeas(ideas, target) {
+  var renderedIdeas = ideas.map(renderIdea);
+  $(target).append(renderedIdeas);
 }
 
-
-// function addIdeasToPage(ideas, target) {
-//   var renderedIdeas = ideas.map(renderIdea);
-//   $(target).append(renderedIdeas);
-// }
-//
-// function fetchIdeasAndAddThemToThePage(target) {
-//     var target = $('.ideas');
-//   return $.getJSON('/ideas').then(function (ideas) {
-//     addIdeasToPage(ideas.idea, target);
-//   });
-// }
+function listAllIdeas(){
+  var target = $('.ideas_list');
+  return $.getJSON('api/v1/ideas.json').then(function (ideas) {
+    collectAndFormatIdeas(ideas, target);
+  });
+}
